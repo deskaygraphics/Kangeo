8a56d8f75db65a89ca2cda00581971abc5c61f5b
Desmond <deskaygraphics@gmail.com>
2026-04-27 20:36:25 -0500

    Apply pre-commit fixes

--- kangeo/__init__.py
+++ kangeo/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Desmond Kangah"""
 __email__ = "deskaygraphics@gmail.com"
 __version__ = "0.0.2"
 
-from .kangeo import *                                                              #help access packages in the kangeo
+from .kangeo import *  # help access packages in the kangeo
