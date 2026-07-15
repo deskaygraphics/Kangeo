7cd526cdc15d14231c177e4d166744a76b447efb
Desmond Kangah <deskaygraphics@gmail.com>
2026-07-15 13:44:46 -0500

    Addmap (#15)
    * add Map class with basemap and Google map functionality
    
    * Apply pre-commit fixes
    
    * Update documentation and development requirements
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- kangeo/__init__.py
+++ kangeo/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Desmond Kangah"""
 __email__ = "deskaygraphics@gmail.com"
 __version__ = "0.0.2"
+
+from .kangeo import *  # help access packages in the kangeo
--- kangeo/kangeo.py
+++ kangeo/kangeo.py
@@ -1,1 +1,27 @@
 """Main module."""
+
+import ipyleaflet
+import ipywidgets as widgets
+
+
+class Map(ipyleaflet.Map):
+
+    def __init__(self, center=[20, 0], zoom=2, height="600px", **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
+        self.layout.height = height
+
+        # Add default basemap
+
+    def add_basemap(self, basemap="OpenTopoMap"):
+        url = eval(f"ipyleaflet.basemaps.{basemap}").build_url()
+        layer = ipyleaflet.TileLayer(url=url, name=basemap)
+        self.add_layer(layer)
+
+    def add_google_map(self, map_type="SATELLITE"):
+
+        map_types = {"SATELLITE": "s", "ROADMAP": "m", "TERRAIN": "t", "HYBRID": "h"}
+        map_type = map_types[map_type.upper()]
+
+        url = f"https://mt1.googleapis.com/vt/lyrs={map_type.lower()}&x={{x}}&y={{y}}&z={{z}}"
+        layer = ipyleaflet.TileLayer(url=url, name=f"Google {map_type}")
+        self.add_layer(layer)
